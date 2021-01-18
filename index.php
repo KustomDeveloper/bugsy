4001427f44e775386b2432c4e0389fb50770b958
Michael Hicks <kustomdesigner@gmail.com>
2021-01-18 09:19:37 -0600

    First Commit

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Woodward Plumbing and Heating</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha512-Dop/vW3iOtayerlYAqCgkVr2aTr2ErwwTYOvRFUpzl2VhCMJyjQF0Q9TjUXIo6JhuM/3i0vVEt2e/7QQmnHQqw==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="./css/style.css" />
+  </head>
+<body>
+  <nav>
+    <img src="./img/logo.png" />
+    <ul>
+      <a href="/"><li>Home</li></a>
+      <a href="services.php"><li>Services</li></a>
+      <a href="about-bill.php"><li>About Bill</li></a>
+    </ul>
+  </nav>
+
+  <a href="#"><button class="cta">CALL NOW</button></a>
+
+  <section>
+    <div class="hero-content">
+      <h1 class="small">Residential Plumbing & Heating Services</h1>
+      <h2>Get it Done Right the First Time</h2>
+      <a href="#"><button class="cta">CALL NOW</button></a>
+    </div>
+  </section>
+
+  <section>
+    <h2>Avoid Paying Twice for the Same Project</h2>
+    <p>Hiring a plumber that has no idea what they are doing is a waste of money. We want to help you do it right the first time.</p>
+    <a href="#"><button class="cta">CALL NOW</button></a>
+  </section>
+
+  <section>
+    <h2>Getting Your Project Taken Care of is Easy...</h2>
+    <div class="col-md-4">
+      <i class="fa fa-phone"></i>
+      <h3>Call Bill</h3>
+      <p>Chat with Bill about your project and what you are looking to have done.</p>
+    </div>
+    <div class="col-md-4">
+      <i class="fa fa-calendar"></i>
+      <h3>Schedule a Visit</h3>
+      <p>Set up a time for Bill to come to your property to assess your project, give you a quote and plan a project date.</p>
+    </div>
+    <div class="col-md-4">
+      <i class="fa fa-home"></i>
+      <h3>Rest Assured</h3>
+      <p>Have peace of mind that your project is going to be handled by a pro!</p>
+    </div>
+
+    <a href="#"><button class="cta">CALL NOW</button></a>
+  </section>
+
+  <section>
+    <div class="col-md-3">
+      <span class="ribbon-icon"></span>
+      <h3>30+ Years Experience</h3>
+    </div>
+    <div class="col-md-3">
+      <span class="money-icon"></span>
+      <h3>Competitive Pricing</h3>
+    </div>
+    <div class="col-md-3">
+      <span class="shield-icon"></span>
+      <h3>Fully Insured</h3>
+    </div>
+    <div class="col-md-3">
+      <span class="check-icon"></span>
+      <h3>NY & PA Licenced</h3>
+    </div>
+
+    <a href="#"><button class="cta">CALL NOW</button></a>
+  </section>
+
+  <section>
+    <h2>We Serve Bradford, Chemung & Tioga Counties</h2>
+
+    <a href="#"><button class="clear-cta">OUR SERVICES</button></a>
+  </section>
+
+  <section>
+    <div class="col-md-3">
+      <p>"Bill is great. Super knowledgeable and great service!"</p>
+      <h3>Lena Hugg</h3>
+    </div>
+    <div class="col-md-3">
+      <p>"Bill made arrangements to come the very next day and replaced my ancient toilet. Then he came back and replaced the foot valve on our well the same week. He is friendly and professional. I am pleased that I called him."</p>
+      <h3>Betty Ling</h3>
+    </div>
+    <div class="col-md-3">
+      <p>“Bill did all the plumbing to get us up and going in your new home. Excellent job!”</p>
+      <h3>Kevin Wood</h3>
+    </div>
+
+    <a href="#"><button class="cta">CALL NOW</button></a>
+  </section>
+  
+  <footer>
+    <div class="col-md-4"><img src="logo-footer" /></div>
+    <div class="col-md-4"><a href="#"><button class="cta">CALL NOW</button></a></div>
+    <div class="col-md-4">SERVING BRADFORD, CHEMUNG & TIOGA COUNTIES</div>
+
+    <div class="copyright">© 2020 All Rights Reserved | Woodward Plumbing and Heating, LLC</div>
+  </footer>
+
+</body>
+</html>
