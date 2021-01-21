71fb879c72b8e1b110af73455ad746ed9b89560d
Michael Hicks <kustomdesigner@gmail.com>
2021-01-21 06:48:16 -0600

    Update theme development

--- index.php
+++ index.php
@@ -4,36 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Woodward Plumbing and Heating</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha512-Dop/vW3iOtayerlYAqCgkVr2aTr2ErwwTYOvRFUpzl2VhCMJyjQF0Q9TjUXIo6JhuM/3i0vVEt2e/7QQmnHQqw==" crossorigin="anonymous" />
     <link rel="stylesheet" href="./css/style.css" />
   </head>
 <body>
-  <nav>
-    <img src="./img/logo.png" />
-    <ul>
-      <a href="/"><li>Home</li></a>
-      <a href="services.php"><li>Services</li></a>
-      <a href="about-bill.php"><li>About Bill</li></a>
-    </ul>
-  </nav>
+  <div class="topbar container">
+    <img class="logo" src="./img/WoodwardPlumbingLogo-480w.png" />
+    <nav>
+      <ul>
+        <a class="selected" href="/"><li>Home</li></a>
+        <a href="services.php"><li>Services</li></a>
+        <a href="about-bill.php"><li>About Bill</li></a>
+      </ul>
+    </nav>
 
-  <a href="#"><button class="cta">CALL NOW</button></a>
+    <a class="cta top-cta" href="tel:6072156508"><button>CALL NOW</button></a>
+  </div><!--.top-bar-->
 
-  <section>
+  <section id="hero">
     <div class="hero-content">
       <h1 class="small">Residential Plumbing & Heating Services</h1>
       <h2>Get it Done Right the First Time</h2>
-      <a href="#"><button class="cta">CALL NOW</button></a>
+      <a class="cta" href="#"><button>CALL NOW</button></a>
     </div>
   </section>
 
-  <section>
+  <section class="container">
     <h2>Avoid Paying Twice for the Same Project</h2>
     <p>Hiring a plumber that has no idea what they are doing is a waste of money. We want to help you do it right the first time.</p>
-    <a href="#"><button class="cta">CALL NOW</button></a>
+    <a class="cta" href="#"><button>CALL NOW</button></a>
   </section>
 
-  <section>
+  <section class="container">
     <h2>Getting Your Project Taken Care of is Easy...</h2>
     <div class="col-md-4">
       <i class="fa fa-phone"></i>
@@ -51,10 +55,10 @@
       <p>Have peace of mind that your project is going to be handled by a pro!</p>
     </div>
 
-    <a href="#"><button class="cta">CALL NOW</button></a>
+    <a class="cta" href="tel:6072156508"><button>CALL NOW</button></a>
   </section>
 
-  <section>
+  <section class="container">
     <div class="col-md-3">
       <span class="ribbon-icon"></span>
       <h3>30+ Years Experience</h3>
@@ -72,16 +76,15 @@
       <h3>NY & PA Licenced</h3>
     </div>
 
-    <a href="#"><button class="cta">CALL NOW</button></a>
+    <a class="cta" href="tel:6072156508"><button>CALL NOW</button></a>
   </section>
 
-  <section>
+  <section class="container">
     <h2>We Serve Bradford, Chemung & Tioga Counties</h2>
-
-    <a href="#"><button class="clear-cta">OUR SERVICES</button></a>
+    <a class="clear-cta" href="/services"><button>OUR SERVICES</button></a>
   </section>
 
-  <section>
+  <section class="container">
     <div class="col-md-3">
       <p>"Bill is great. Super knowledgeable and great service!"</p>
       <h3>Lena Hugg</h3>
@@ -95,12 +98,12 @@
       <h3>Kevin Wood</h3>
     </div>
 
-    <a href="#"><button class="cta">CALL NOW</button></a>
+    <a class="cta" href="tel:6072156508"><button>CALL NOW</button></a>
   </section>
   
-  <footer>
-    <div class="col-md-4"><img src="logo-footer" /></div>
-    <div class="col-md-4"><a href="#"><button class="cta">CALL NOW</button></a></div>
+  <footer class="container">
+    <div class="col-md-4"><img class="logo" src="./img/WoodwardPlumbingLogo-480w.png" /></div>
+    <div class="col-md-4"><a class="cta" href="tel:6072156508"><button>CALL NOW</button></a></div>
     <div class="col-md-4">SERVING BRADFORD, CHEMUNG & TIOGA COUNTIES</div>
 
     <div class="copyright">© 2020 All Rights Reserved | Woodward Plumbing and Heating, LLC</div>
