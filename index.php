7fb6e76636cdd2d48d0e62e07922f58a475ddb6b
Michael Hicks <kustomdesigner@gmail.com>
2021-02-04 08:21:51 -0600

    Still need to finish footer and mobile

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-  <!-- <meta http-equiv="refresh" content="3; url="<?php //echo $_SERVER['PHP_SELF']; ?>"> -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Woodward Plumbing and Heating</title>
-   <!-- Latest compiled and minified CSS -->
+
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/font-awesome.min.css" />
     <link rel="stylesheet" href="./css/style.css" />
   </head>
 <body>
   <div class="topbar container">
     <img class="logo" src="./img/WoodwardPlumbingLogo-480w.png" />
+    <div class="mobile-menu"><i class="fa fa-menu"></i></div>
     <nav>
       <ul>
         <a class="selected" href="/"><li>Home</li></a>
@@ -27,89 +28,154 @@
     <div class="hero-content">
       <h1 class="smallHeadline">Residential Plumbing & Heating Services</h1>
       <h2>Get it Done Right the First Time</h2>
-      <a class="cta" href="#"><button>CALL NOW</button></a>
+      <a class="cta" href="tel:6072156508"><button>CALL NOW</button></a>
     </div>
   </section>
 
-  <section class="container">
-    <h2>Avoid Paying Twice for the Same Project</h2>
-    <p>Hiring a plumber that has no idea what they are doing is a waste of money. We want to help you do it right the first time.</p>
-    <a class="cta" href="#"><button>CALL NOW</button></a>
+  <section class="section1">
+    <div class="container">
+      <h2>Avoid Paying Twice for the Same Project</h2>
+      <p>Hiring a plumber that has no idea what they are doing is a waste of money. We want to help you do it right the first time.</p>
+      <a class="cta" href="tel:6072156508"><button>CALL NOW</button></a>
+    </div>
   </section>
 
-  <section class="container">
-    <div class="row">
-      <h2>Getting Your Project Taken Care of is Easy...</h2>
-      <div class="col-md-4">
-        <i class="fa fa-phone"></i>
-        <h3>Call Bill</h3>
-        <p>Chat with Bill about your project and what you are looking to have done.</p>
+  <section class="section2">
+  <div class="container">
+      <div class="row">
+        <h2>Getting Your Project Taken Care of is Easy...</h2>
+        <div class="col-md-4">
+        <svg class="phone-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 70 70" id="1299833985" data-icon-name="wp-phone"> <g id="1506316864"> <path d="M25.3,44.8C35.7,55.3,44.2,56,46.6,56c3.9,0,9.4-5,9.4-6.3v-2.5c0-0.6-0.4-1.1-1-1.3l-10-3.2c-0.5-0.2-1,0-1.4,0.3L40,46.8
+      c-0.4,0.4-1.1,0.5-1.6,0.2c-1.5-0.9-4.8-2.9-8.6-6.8c-3.8-3.8-5.9-7.1-6.8-8.6c-0.3-0.5-0.2-1.2,0.2-1.6l3.7-3.7
+      c0.4-0.4,0.5-0.9,0.3-1.4l-3.2-10c-0.2-0.6-0.7-1-1.3-1h-2.5C19,14.1,14,19.7,14,23.5C14,25.8,14.8,34.3,25.3,44.8L25.3,44.8z" id="1085426415"></path></g></svg>
+          <!-- <i class="fa fa-phone"></i> -->
+          <h3>Call Bill</h3>
+          <p>Chat with Bill about your project and what you are looking to have done.</p>
+        </div>
+        <div class="col-md-4">
+          <svg class="calendar-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" id="1621523132" data-icon-name="business_calendar_checkmark"> <path d="M60.75,17.88A21.4,21.4,0,0,0,39.41,38h-8V34.5a1.3,1.3,0,1,0-2.59,0V38H22.87a5,5,0,0,0-5,5V77.13a5,5,0,0,0,5,5H57.05a5,5,0,0,0,5-5V60.59a21.37,21.37,0,0,0-1.3-42.71ZM22.87,40.55H28.8V44a1.3,1.3,0,1,0,2.59,0V40.55h8a21.21,21.21,0,0,0,1.65,7H20.47V42.95A2.4,2.4,0,0,1,22.87,40.55ZM59.45,77.13a2.4,2.4,0,0,1-2.4,2.4H22.87a2.4,2.4,0,0,1-2.4-2.4v-27H42.38a21.51,21.51,0,0,0,4.25,5.12,3.12,3.12,0,0,0-.11.83v3.95a3.15,3.15,0,0,0,3.14,3.14h3.95a3.15,3.15,0,0,0,3.14-3,21.33,21.33,0,0,0,2.7.34ZM54.16,59.59v.47a.56.56,0,0,1-.55.55H49.67a.56.56,0,0,1-.55-.55V57.19A21.12,21.12,0,0,0,54.16,59.59ZM60.75,58a18.72,18.72,0,0,1-5.36-.78l-.3-.09-.67-.23-.17-.06-.78-.31,0,0A18.78,18.78,0,1,1,60.75,58Z" id="1515014037"></path> 
+          <path d="M55.92,49.83a4.47,4.47,0,0,1-3.17-1.31L47.25,43a1.3,1.3,0,0,1,1.83-1.83l5.5,5.5a1.89,1.89,0,0,0,2.67,0L72.41,31.53a1.3,1.3,0,0,1,1.83,1.83L59.09,48.52A4.47,4.47,0,0,1,55.92,49.83Z" id="1844943003"></path> 
+          <path d="M29.51,63.2H27.06a3.9,3.9,0,0,1-3.89-3.89V56.86A3.9,3.9,0,0,1,27.06,53h2.45a3.9,3.9,0,0,1,3.89,3.89v2.45A3.9,3.9,0,0,1,29.51,63.2Zm-2.45-7.64a1.3,1.3,0,0,0-1.3,1.3v2.45a1.3,1.3,0,0,0,1.3,1.3h2.45a1.3,1.3,0,0,0,1.3-1.3V56.86a1.3,1.3,0,0,0-1.3-1.3Z" id="1894339672"></path> 
+          <path d="M29.51,75.37H27.06a3.9,3.9,0,0,1-3.89-3.89V69a3.9,3.9,0,0,1,3.89-3.89h2.45A3.9,3.9,0,0,1,33.4,69v2.45A3.9,3.9,0,0,1,29.51,75.37Zm-2.45-7.64a1.3,1.3,0,0,0-1.3,1.3v2.45a1.3,1.3,0,0,0,1.3,1.3h2.45a1.3,1.3,0,0,0,1.3-1.3V69a1.3,1.3,0,0,0-1.3-1.3Z" id="1186869286"></path> 
+          <path d="M41.19,63.2H38.73a3.9,3.9,0,0,1-3.89-3.89V56.86A3.9,3.9,0,0,1,38.73,53h2.45a3.9,3.9,0,0,1,3.89,3.89v2.45A3.9,3.9,0,0,1,41.19,63.2Zm-2.45-7.64a1.3,1.3,0,0,0-1.3,1.3v2.45a1.3,1.3,0,0,0,1.3,1.3h2.45a1.3,1.3,0,0,0,1.3-1.3V56.86a1.3,1.3,0,0,0-1.3-1.3Z" id="1888508288"></path> 
+          <path d="M41.19,75.37H38.73a3.9,3.9,0,0,1-3.89-3.89V69a3.9,3.9,0,0,1,3.89-3.89h2.45A3.9,3.9,0,0,1,45.08,69v2.45A3.9,3.9,0,0,1,41.19,75.37Zm-2.45-7.64a1.3,1.3,0,0,0-1.3,1.3v2.45a1.3,1.3,0,0,0,1.3,1.3h2.45a1.3,1.3,0,0,0,1.3-1.3V69a1.3,1.3,0,0,0-1.3-1.3Z" id="1813125218"></path> 
+          <path d="M52.87,75.37H50.41a3.9,3.9,0,0,1-3.89-3.89V69a3.9,3.9,0,0,1,3.89-3.89h2.45A3.9,3.9,0,0,1,56.76,69v2.45A3.9,3.9,0,0,1,52.87,75.37Zm-2.45-7.64a1.3,1.3,0,0,0-1.3,1.3v2.45a1.3,1.3,0,0,0,1.3,1.3h2.45a1.3,1.3,0,0,0,1.3-1.3V69a1.3,1.3,0,0,0-1.3-1.3Z" id="1092968621"></path> 
+        </svg>
+          <h3>Schedule a Visit</h3>
+          <p>Set up a time for Bill to come to your property to assess your project, give you a quote and plan a project date.</p>
+        </div>
+        <div class="col-md-4">
+          <svg class="house-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" id="1126757015" data-icon-name="li-house"> <path d="M81.7,38.8L50.6,18c-0.3-0.2-0.8-0.2-1.2,0L18.3,38.8c-0.4,0.3-0.6,0.7-0.4,1.2c0.1,0.4,0.5,0.7,1,0.7h9.3v40.5
+          c0,0.6,0.5,1,1,1h10.4h20.8h10.4c0.6,0,1-0.5,1-1V40.7h9.3c0.5,0,0.9-0.3,1-0.7C82.3,39.5,82.1,39,81.7,38.8z M40.7,80.1V51h18.7
+          v29.1H40.7z M70.8,38.6c-0.6,0-1,0.5-1,1v40.5h-8.3V50c0-0.6-0.5-1-1-1H39.6c-0.6,0-1,0.5-1,1v30.1h-8.3V39.6c0-0.6-0.5-1-1-1h-7
+          L50,20.1l27.7,18.5H70.8z" id="1707175720"></path> 
+          </svg>
+          <h3>Rest Assured</h3>
+          <p>Have peace of mind that your project is going to be handled by a pro!</p>
+        </div>
       </div>
-      <div class="col-md-4">
-        <i class="fa fa-calendar"></i>
-        <h3>Schedule a Visit</h3>
-        <p>Set up a time for Bill to come to your property to assess your project, give you a quote and plan a project date.</p>
+
+      <a class="cta" href="tel:6072156508"><button>CALL NOW</button></a>
+    </div>
+  </section>
+
+  <section class="container section3">
+  <div class="row">
+      <div class="col-md-3">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" id="1485924506" class="svg u_1485924506" data-icon-name="li_award"> <g id="1941353101"> <path d="M50,23.5c-11,0-19.9,8.9-19.9,19.9S39,63.3,50,63.3s19.9-8.9,19.9-19.9S61,23.5,50,23.5z M50,60.9
+        c-9.6,0-17.5-7.8-17.5-17.5C32.5,33.8,40.4,26,50,26s17.5,7.8,17.5,17.5C67.5,53.1,59.6,60.9,50,60.9z" id="1635600008"></path> 
+          <path d="M88.3,73.7L75.6,61l2.2-1.3V51l4.4-7.6l-4.4-7.6v-8.7l-7.6-4.4l-4.4-7.6h-8.7l-7.6-4.4L42,15.2h-8.7l-4.4,7.6l-7.6,4.4v8.7
+              L17,43.4l4.4,7.6v8.7l2.7,1.6L11.7,73.7c-0.4,0.4-0.5,0.9-0.3,1.4s0.6,0.8,1.2,0.8h11.2v11.2c0,0.5,0.3,1,0.8,1.2
+              c0.2,0.1,0.3,0.1,0.5,0.1c0.3,0,0.6-0.1,0.9-0.4l16.2-16.2l7.5,4.3l7.6-4.4h0.6L74.2,88c0.2,0.2,0.6,0.4,0.9,0.4
+              c0.2,0,0.3,0,0.5-0.1c0.5-0.2,0.8-0.6,0.8-1.2V75.8h11.2c0.5,0,1-0.3,1.2-0.8C88.7,74.6,88.6,74.1,88.3,73.7z M25.1,73.4h-9.5
+              l10.7-10.7l2.6,1.5l4.4,7.6h5.4L26.3,84.1v-9.5C26.3,73.9,25.8,73.4,25.1,73.4z M56.5,69.2l-6.9,4l-6.9-4h-8l-4-6.9l-6.9-4v-8
+              l-4-6.9l4-6.9v-8l6.9-4l4-6.9h8l6.9-4l6.9,4h8l4,6.9l6.9,4v8l4,6.9l-4,6.9v8l-6.9,4l-4,6.9H56.5z M74.9,73.4
+              c-0.7,0-1.2,0.6-1.2,1.2v9.5L61.3,71.7h4.6l4.4-7.6l3.1-1.8l11,11L74.9,73.4L74.9,73.4z" id="1513014444"></path> 
+          </g> 
+          </svg>
+        <h3>30+ Years Experience</h3>
       </div>
-      <div class="col-md-4">
-        <i class="fa fa-home"></i>
-        <h3>Rest Assured</h3>
-        <p>Have peace of mind that your project is going to be handled by a pro!</p>
+      <div class="col-md-3">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 70 70" id="1383244431" class="svg u_1383244431" data-icon-name="wp-money_thin"> <g id="1025111543"> <path d="M35,10c-13.8,0-25,11.2-25,25s11.2,25,25,25s25-11.2,25-25S48.8,10,35,10z M35,58.1c-12.7,0-23.1-10.4-23.1-23.1
+                  S22.3,11.9,35,11.9c12.7,0,23.1,10.4,23.1,23.1S47.7,58.1,35,58.1z" id="1147027297"></path> 
+              <path d="M35.3,33.5c-4-1.2-5.3-2-5.3-3.7c0-2.3,2.7-2.8,5-2.8c3.6,0,4.7,2.3,4.8,2.4c0.2,0.5,0.8,0.7,1.3,0.5
+              c0.5-0.2,0.7-0.8,0.5-1.3c-0.1-0.1-1.4-3.1-5.6-3.5V23c0-0.6-0.4-1-1-1c-0.6,0-1,0.4-1,1v2c-5.4,0.3-6,3.4-6,4.8
+              c0,3.4,3.2,4.6,6.7,5.6C40,36.9,40,39,40,39.7c0,1.6-0.6,3.3-5.3,3.3c-4.1,0-4.7-2.4-4.8-2.5c-0.1-0.5-0.6-0.9-1.2-0.8
+              c-0.5,0.1-0.9,0.6-0.8,1.2c0,0.2,0.8,3.8,6,4.1v2c0,0.6,0.4,1,1,1c0.6,0,1-0.4,1-1v-2.1c4.9-0.4,6-3,6-5.3
+              C42,36.8,39.8,34.8,35.3,33.5z" id="1441772891"></path> 
+          </g> 
+          </svg>
+        <h3>Competitive Pricing</h3>
+      </div>
+      <div class="col-md-3">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 70 70" id="1525931174" class="svg u_1525931174" data-icon-name="wp-security_thin"> <path d="M53.3,16C53.3,16,53.2,16,53.3,16c-1.1,0-10.3-0.6-16.5-5.4C36.3,10.2,35.7,10,35,10c-0.6,0-1.3,0.2-1.8,0.6
+          C27,15.5,17.8,16,16.7,16c-1.6,0.1-2.7,1.4-2.7,3v12c0,19.7,19,28.4,19.8,28.7c0.4,0.2,0.8,0.3,1.2,0.3s0.8-0.1,1.2-0.3
+          C37,59.4,56,50.7,56,31V19C56,17.4,54.8,16.2,53.3,16z M53.1,18c0.5,0.1,0.9,0.5,0.9,1v12c0,0.3,0,0.7,0,1H36V12.5
+          C42.5,17.3,51.3,17.9,53.1,18z M16,19c0-0.5,0.4-0.9,0.9-1c1.7-0.1,10.6-0.7,17.1-5.5V32H16c0-0.3,0-0.7,0-1V19z M16.2,34H34v23.6
+          C31,56.1,17.8,48.8,16.2,34z M36,57.6V34h17.8C52.2,48.8,39,56.1,36,57.6z" id="1763301949"></path> 
+        </svg>
+        <h3>Fully Insured</h3>
+      </div>
+      <div class="col-md-3">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 70 70" id="1264725755" class="svg u_1264725755" data-icon-name="wp-checked_icon"> <g id="1272995564"> <path d="M35,60c-13.8,0-25-11.2-25-25s11.2-25,25-25s25,11.2,25,25S48.8,60,35,60z M35,12c-12.7,0-23,10.3-23,23s10.3,23,23,23
+            s23-10.3,23-23S47.7,12,35,12z" id="1561761450"></path> 
+        <polygon points="32,43.4 23.3,34.7 24.7,33.3 32,40.6 48.3,24.3 49.7,25.7 	" id="1149979730"></polygon> 
+        </g> 
+        </svg>
+        <h3>NY & PA Licenced</h3>
       </div>
     </div>
 
     <a class="cta" href="tel:6072156508"><button>CALL NOW</button></a>
   </section>
 
-  <section class="container">
-    <div class="col-md-3">
-      <span class="ribbon-icon"></span>
-      <h3>30+ Years Experience</h3>
+  <section class="section4">
+    <div class="container">
+      <h2>We Serve Bradford, Chemung & Tioga Counties</h2>
+      <a class="clear-cta" href="/services"><button>OUR SERVICES</button></a>
     </div>
-    <div class="col-md-3">
-      <span class="money-icon"></span>
-      <h3>Competitive Pricing</h3>
-    </div>
-    <div class="col-md-3">
-      <span class="shield-icon"></span>
-      <h3>Fully Insured</h3>
-    </div>
-    <div class="col-md-3">
-      <span class="check-icon"></span>
-      <h3>NY & PA Licenced</h3>
-    </div>
-
-    <a class="cta" href="tel:6072156508"><button>CALL NOW</button></a>
   </section>
 
-  <section class="container">
-    <h2>We Serve Bradford, Chemung & Tioga Counties</h2>
-    <a class="clear-cta" href="/services"><button>OUR SERVICES</button></a>
-  </section>
-
-  <section class="container">
-    <div class="col-md-3">
-      <p>"Bill is great. Super knowledgeable and great service!"</p>
-      <h3>Lena Hugg</h3>
+  <section class="section5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1664 1792" id="1160086903" class="svg u_1160086903" data-icon-name="fa-quote-left"> <path fill="inherit" d="M768 960v384q0 80-56 136t-136 56h-384q-80 0-136-56t-56-136v-704q0-104 40.5-198.5t109.5-163.5 163.5-109.5 198.5-40.5h64q26 0 45 19t19 45v128q0 26-19 45t-45 19h-64q-106 0-181 75t-75 181v32q0 40 28 68t68 28h224q80 0 136 56t56 136zM1664 960v384q0 80-56 136t-136 56h-384q-80 0-136-56t-56-136v-704q0-104 40.5-198.5t109.5-163.5 163.5-109.5 198.5-40.5h64q26 0 45 19t19 45v128q0 26-19 45t-45 19h-64q-106 0-181 75t-75 181v32q0 40 28 68t68 28h224q80 0 136 56t56 136z" id="1672845396"></path> 
+          </svg>
+          <p>"Bill is great. Super knowledgeable and great service!"</p>
+          <h3>Lena Hugg</h3>
+        </div>
+        <div class="col-md-4">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1664 1792" id="1160086903" class="svg u_1160086903" data-icon-name="fa-quote-left"> <path fill="inherit" d="M768 960v384q0 80-56 136t-136 56h-384q-80 0-136-56t-56-136v-704q0-104 40.5-198.5t109.5-163.5 163.5-109.5 198.5-40.5h64q26 0 45 19t19 45v128q0 26-19 45t-45 19h-64q-106 0-181 75t-75 181v32q0 40 28 68t68 28h224q80 0 136 56t56 136zM1664 960v384q0 80-56 136t-136 56h-384q-80 0-136-56t-56-136v-704q0-104 40.5-198.5t109.5-163.5 163.5-109.5 198.5-40.5h64q26 0 45 19t19 45v128q0 26-19 45t-45 19h-64q-106 0-181 75t-75 181v32q0 40 28 68t68 28h224q80 0 136 56t56 136z" id="1672845396"></path> 
+          </svg>
+          <p>"Bill made arrangements to come the very next day and replaced my ancient toilet. Then he came back and replaced the foot valve on our well the same week. He is friendly and professional. I am pleased that I called him."</p>
+          <h3>Betty Ling</h3>
+        </div>
+        <div class="col-md-4">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1664 1792" id="1160086903" class="svg u_1160086903" data-icon-name="fa-quote-left"> <path fill="inherit" d="M768 960v384q0 80-56 136t-136 56h-384q-80 0-136-56t-56-136v-704q0-104 40.5-198.5t109.5-163.5 163.5-109.5 198.5-40.5h64q26 0 45 19t19 45v128q0 26-19 45t-45 19h-64q-106 0-181 75t-75 181v32q0 40 28 68t68 28h224q80 0 136 56t56 136zM1664 960v384q0 80-56 136t-136 56h-384q-80 0-136-56t-56-136v-704q0-104 40.5-198.5t109.5-163.5 163.5-109.5 198.5-40.5h64q26 0 45 19t19 45v128q0 26-19 45t-45 19h-64q-106 0-181 75t-75 181v32q0 40 28 68t68 28h224q80 0 136 56t56 136z" id="1672845396"></path> 
+          </svg>
+          <p>“Bill did all the plumbing to get us up and going in your new home. Excellent job!”</p>
+          <h3>Kevin Wood</h3>
+        </div>
+      </div>
     </div>
-    <div class="col-md-3">
-      <p>"Bill made arrangements to come the very next day and replaced my ancient toilet. Then he came back and replaced the foot valve on our well the same week. He is friendly and professional. I am pleased that I called him."</p>
-      <h3>Betty Ling</h3>
-    </div>
-    <div class="col-md-3">
-      <p>“Bill did all the plumbing to get us up and going in your new home. Excellent job!”</p>
-      <h3>Kevin Wood</h3>
-    </div>
-
     <a class="cta" href="tel:6072156508"><button>CALL NOW</button></a>
   </section>
   
-  <footer class="container">
-    <div class="col-md-4"><img class="logo" src="./img/WoodwardPlumbingLogo-480w.png" /></div>
-    <div class="col-md-4"><a class="cta" href="tel:6072156508"><button>CALL NOW</button></a></div>
-    <div class="col-md-4">SERVING BRADFORD, CHEMUNG & TIOGA COUNTIES</div>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4"><img class="logo" src="./img/WoodwardPlumbingLogo-480w.png" /></div>
+        <div class="col-md-4"><h3>Location</h3><p>SERVING BRADFORD, CHEMUNG & TIOGA COUNTIES</p></div>
+        <div class="col-md-4"><h3>Contact Me</h3><p><i class="fa fa-facebook"></i><i class="fa fa-email"></i></p></div>
+      </div>
+    </div>
 
     <div class="copyright">© 2020 All Rights Reserved | Woodward Plumbing and Heating, LLC</div>
   </footer>
 
+  <!--Scripts go here-->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.0/gsap.min.js"></script>
+  <script src="./js/main.js"></script>
 </body>
 </html>
